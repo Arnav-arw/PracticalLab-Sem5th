bf5a0a8c12c4f98228618e86896eae96670f414a
Arnav Singhal <singhal.arnav@gmail.com>
2023-08-10 10:24:46 +0530

    selection sort added

---
+++ DAA /selectionSort.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main() {
+    int n;
+    int i, j, key;
+    
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+ 
+    int arr[n];
+    printf("Enter your array: ");
+    for(int i=0; i<=n-1; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for(i=0; i<n-1; i++) {
+        for(j=i+1; j<n; j++) {
+            if(arr[i]>arr[j]) {
+                key = arr[i];
+                arr[i] = arr[j];
+                arr[j] = key;
+            }
+        }
+    }
+
+    printf("Sorted Array: ");
+    for(i=0; i<n; i++) {
+        printf("%d ", arr[i]);
+    }
+}
